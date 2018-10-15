24ad7db376c430ce59cc295cde5f2c62cd5d0d30
M.C <hanhchaumn@gmail.com>
2018-10-15 17:22:10 +0700

    Add bias to current location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { RoutingService } from './routing.service';
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { MouseEvent, MapsAPILoader, AgmMap } from '@agm/core';
+import { MouseEvent, MapsAPILoader, AgmMap, LatLngLiteral } from '@agm/core';
+import { Observable, Observer } from 'rxjs';
 
 interface Map {
     lat: number;
@@ -34,6 +35,12 @@ export class AppComponent {
         private mapsAPILoader: MapsAPILoader
     ) {
         this.routingService.startObservingUrls();
+        this.marker = {
+            lat: 0,
+            lng: 0,
+            draggable: true
+        };
+
         this.map = {
             lat: 0,
             lng: 0,
@@ -43,40 +50,67 @@ export class AppComponent {
 
     ngOnInit(): void {
         this.mapsAPILoader.load().then(() => {
-            const autocomplete = new google.maps.places.Autocomplete(
-                this.addressBoxRef.nativeElement,
-                {
-                    types: []
-                }
-            );
+            this.getCurrentPosition()
+            .subscribe((latLng: LatLngLiteral) => {
+                this.updateMarker(latLng.lat, latLng.lng);
+                this.updateMap(latLng.lat, latLng.lng, 12);
 
-            autocomplete.addListener('place_changed', () => {
-                const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+                const bounds = new google.maps.Circle({
+                    center: latLng,
+                    radius: 25
+                });
 
-                if (place.geometry) {
-                    const lat = place.geometry.location.lat();
-                    const lng = place.geometry.location.lng();
-                    this.marker.lat = lat;
-                    this.marker.lng = lng;
-                    this.map.lat = this.marker.lat;
-                    this.map.lng = this.marker.lng;
-                    this.map.zoom = 17;
-                    this.agmMapRef.triggerResize(true).then(() => {
-                        (this.agmMapRef as any)._mapsWrapper.setCenter({
-                            lat,
-                            lng
+                const autocomplete = new google.maps.places.Autocomplete(
+                    this.addressBoxRef.nativeElement,
+                    {
+                        types: [],
+                        bounds: bounds.getBounds()
+                    }
+                );
+
+                autocomplete.addListener('place_changed', () => {
+                    const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+                    if (place.geometry) {
+                        const lat = place.geometry.location.lat();
+                        const lng = place.geometry.location.lng();
+                        this.marker.lat = lat;
+                        this.marker.lng = lng;
+                        this.map.lat = this.marker.lat;
+                        this.map.lng = this.marker.lng;
+                        this.map.zoom = 17;
+                        this.agmMapRef.triggerResize(true).then(() => {
+                            (this.agmMapRef as any)._mapsWrapper.setCenter({
+                                lat,
+                                lng
+                            });
                         });
-                    });
-                }
+                    }
+                });
             });
+        });
+    }
 
-            this.setCurrentPosition();
+    updateMarker(lat: number, lng: number) {
+        this.marker.lat = lat;
+        this.marker.lng = lng;
+    }
+
+    updateMap(lat: number, lng: number, zoom?: number) {
+        this.map.lat = lat;
+        this.map.lng = lng;
+        this.map.zoom = zoom ? zoom : this.map.zoom;
+        this.agmMapRef.triggerResize(true).then(() => {
+            (this.agmMapRef as any)._mapsWrapper.setCenter({
+                lat,
+                lng
+            });
         });
     }
 
     updateMarkerPosition($event: MouseEvent) {
         const { lat, lng } = { ...$event.coords };
-        this.marker = { lat, lng, draggable: true };
+        this.updateMarker(lat, lng);
     }
 
     mapClicked($event: MouseEvent) {
@@ -87,15 +121,19 @@ export class AppComponent {
         this.updateMarkerPosition($event);
     }
 
-    private setCurrentPosition() {
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition((position: Position) => {
-                const lat = position.coords.latitude;
-                const lng = position.coords.longitude;
-                this.marker = { lat, lng };
-                this.map.lat = lat;
-                this.map.lng = lng;
-            });
-        }
+    private getCurrentPosition(): Observable<LatLngLiteral> {
+        return Observable.create((observer: Observer<LatLngLiteral>) => {
+            if ('geolocation' in navigator) {
+                navigator.geolocation.getCurrentPosition((position: Position) => {
+                    const lat = position.coords.latitude;
+                    const lng = position.coords.longitude;
+                    const latLng: LatLngLiteral = {
+                        lat,
+                        lng
+                    };
+                    observer.next(latLng);
+                });
+            }
+        });
     }
 }
