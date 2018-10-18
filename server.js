04f87f311ecef17c0fb4603fe53f8446827f5aa5
M.C <hanhchaumn@gmail.com>
2018-10-18 14:50:10 +0700

    Fix old imports

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
-const consts = require('./src/consts');
 
 // the __dirname is the current directory from where the script is running
 const dist = path.resolve(__dirname, "dist");
